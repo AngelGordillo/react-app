d3adf09917b0e8b1bf0e1e25d53929fe6f01af4b
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-10-31 17:21:37 +0100

    building my first app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var todos = [
+  {
+    todoTitle: 'My first todo',
+    todoResponsible: 'Angel',
+    todoDescription: 'My first description',
+    todoPriority: 'low',
+  },
+    {
+    todoTitle: 'My second todo',
+    todoResponsible: 'Angel',
+    todoDescription: 'My second description',
+    todoPriority: 'medium',
+  },
+    {
+    todoTitle: 'My third todo',
+    todoResponsible: 'Angel',
+    todoDescription: 'My third description',
+    todoPriority: 'high',
+  }
+]
 class App extends Component {
+//set the initial component
+  constructor(props){
+    super(props);
+    this.state = {
+      todos
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div className="container">  
+      <h4>Todo Count: <span className="badge"> {this.state.todos.length} </span> </h4> 
+
+     </div>
     );
   }
 }
