629ac96823042c611ae76959836389c262e0f831
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-11-02 18:59:29 +0100

    preared for working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TodoInput from './TodoInput';
 var todos = [
   {
     todoTitle: 'My first todo',
@@ -29,15 +29,42 @@ class App extends Component {
     this.state = {
       todos
     };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
+  handleRemoveTodo(index){
+    this.setState({
+      todos: this.state.todos.filter(function(e, i){
+        return i !== index;
+      })
+    })
+  }
+
+  handleAddTodo(todo){
+    this.setState({todos: [...this.state.todos, todo]})
+  }
+
   render() {
     return (
      <div className="container">  
+     <TodoInput onAddTodo={this.handleAddTodo} > </TodoInput>
+     <hr/>
       <h4>Todo Count: <span className="badge"> {this.state.todos.length} </span> </h4> 
-
+      <ul className="list-group">
+        {this.state.todos.map((todo, index) =>
+          <li className="list-group-item" key={index}>
+            <h4 className="list-group-item-heading"> {todo.todoTitle} <small> <span className="label label-info">{todo.todoPriority} </span> </small> </h4>
+              <p><span className="glyphicon glyphicon-user"> </span>
+            {todo.todoResponsible}</p>
+              <p>{todo.todoDescription}</p>
+              <button className="btn btn-danger btn-sm" onClick={this.handleRemoveTodo.bind(this, index)} ><span className=" glyphicon glyphicon-trash"></span>Delete</button>
+          </li>
+          )}
+      </ul>   
      </div>
     );
   }
 }
+
+
 
 export default App;
